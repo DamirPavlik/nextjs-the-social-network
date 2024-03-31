42b66bbe95650a7ba6c225620d53090c51cdccc0
DamirPavlik <damir.pavlik@gmail.com>
2024-03-31 13:53:29 +0200

    feat: added auth context to keep user singed in

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/firebase/config";
+
+const AuthProvider: React.FC<any> = ({ children }) => {
+  const [userData, setUserData] = useState<any>();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserData(user);
+      console.log("alooooooooooooo");
+    } else {
+      console.log("singed out");
+    }
+  });
+
+  return (
+    <AuthContext.Provider value={userData}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
