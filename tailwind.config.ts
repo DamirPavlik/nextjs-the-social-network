6195b64b96d5efa32117cef0dbd1b07aad59a27f
DamirPavlik <damir.pavlik@gmail.com>
2024-03-31 13:53:01 +0200

    style: added main colors to tailwind.config and clean up of default next css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      "primary-bg": "#22272e",
+      "primary-bg-tint": "#2d333b",
+      "primary-bg-shade": "#1c2128",
+      "primary-text": "#c5d1de",
+      "primary-blue": "#539bf5",
+      "border-color": "#444c56",
     },
   },
   plugins: [],
