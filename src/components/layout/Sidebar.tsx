5a1bc45ab88f06ed6b08c3942f0d7f6c29cbb513
DamirPavlik <damir.pavlik@gmail.com>
2024-04-12 23:01:09 +0200

    feat: added profile picture in the sidebar and a link to users profile

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,10 +1,16 @@
 import { UserProps } from "@/types";
+import Link from "next/link";
 
 const Sidebar: React.FC<UserProps> = ({ user }) => {
   return (
     <section>
+      <div>
+        photo:
+        <img src={String(user?.photoURL)} alt="" />
+      </div>
       <h6>Username: {user?.displayName}</h6>
       <h5>Email: {user?.email}</h5>
+      <Link href={`users/${user?.displayName}`}>View My Profile</Link>
     </section>
   );
 };
