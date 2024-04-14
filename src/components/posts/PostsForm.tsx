0b95a0704147201e8da5a258cdb9884d5519a882
DamirPavlik <damir.pavlik@gmail.com>
2024-04-15 00:01:20 +0200

    style: updated Posts Form styling

--- src/components/posts/PostsForm.tsx
+++ src/components/posts/PostsForm.tsx
@@ -6,7 +6,7 @@ import { revalidatePath } from "next/cache";
 const PostsForm: React.FC<UserProps> = ({ user }) => {
   const username = String(user?.displayName);
   return (
-    <section>
+    <section className="mt-16 mb-6">
       <form
         action={async (formData) => {
           "use server";
@@ -14,10 +14,7 @@ const PostsForm: React.FC<UserProps> = ({ user }) => {
           revalidatePath("/");
         }}
       >
-        <div>
-          <label htmlFor="title" className="block">
-            Title
-          </label>
+        <div className="mb-4">
           <input
             type="text"
             name="title"
@@ -26,10 +23,7 @@ const PostsForm: React.FC<UserProps> = ({ user }) => {
             className="w-full bg-primary-bg border-2 border-border-color rounded-md px-4 pt-[10px] pb-[9px]"
           />
         </div>
-        <div>
-          <label htmlFor="content" className="block">
-            Content
-          </label>
+        <div className="mb-2">
           <textarea
             name="content"
             id="content"
@@ -40,7 +34,13 @@ const PostsForm: React.FC<UserProps> = ({ user }) => {
           ></textarea>
         </div>
         <input type="hidden" name="username" id="username" value={username} />
-        <Button title="Submit" buttonType="submit" />
+        <div className="text-right">
+          <Button
+            title="Submit"
+            buttonType="submit"
+            containerStyles="pt-[8px] pb-[7px] px-5"
+          />
+        </div>
       </form>
     </section>
   );
