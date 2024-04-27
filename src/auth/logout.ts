5cae649e56da45743ad34382aac4899a8af54fcb
DamirPavlik <damir.pavlik@gmail.com>
2024-04-28 00:16:15 +0200

    feat: logout function removes error cookies

--- src/auth/logout.ts
+++ src/auth/logout.ts
@@ -4,5 +4,7 @@ import { cookies } from "next/headers";
 
 export async function logout() {
   cookies().delete("session");
+  cookies().delete("loginError");
+  cookies().delete("registerError");
   signOut(auth);
 }
