3a999f5cdb7d96f5eb854a18505f2b0a809479ea
DamirPavlik <damir.pavlik@gmail.com>
2024-05-10 00:58:39 +0200

    feat: added delete button

--- src/app/my-profile/[slug]/page.tsx
+++ src/app/my-profile/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getSession } from "@/auth/getSession";
-import { Button, Header } from "@/components";
+import { removeUser } from "@/auth/removeUser";
+import { Button, Header, UserPosts } from "@/components";
 import { auth } from "@/firebase/config";
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 const UserProfile = async ({ params }: { params: { slug: string } }) => {
@@ -15,7 +15,7 @@ const UserProfile = async ({ params }: { params: { slug: string } }) => {
     <>
       <Header />
       <div className="max-w-[1000px] block mx-auto mt-8">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mb-10">
           <div className="flex items-center">
             <img
               src={String(user?.photoURL)}
@@ -38,14 +38,31 @@ const UserProfile = async ({ params }: { params: { slug: string } }) => {
                 "use server";
                 redirect(`/my-profile/${params.slug}/edit`);
               }}
+              className="mb-3"
             >
               <Button
                 title="Edit Profile"
                 buttonType="submit"
-                containerStyles="py-2 px-6"
+                containerStyles="py-2 px-14"
+              />
+            </form>
+            <form
+              action={async () => {
+                "use server";
+                await removeUser();
+              }}
+            >
+              <Button
+                title="Delete My Account"
+                buttonType="submit"
+                containerStyles="py-2 px-6 !border-[#CA0B00]"
               />
             </form>
           </div>
+        </div>
+        <hr className="bg-primary-bg-tint text-primary-bg-tint h-1" />
+        <div className="mt-10">
+          <UserPosts />
         </div>
       </div>
     </>
