a5a8b5cbe765bb3a9409c53432c5d6bc8d2073fc
DamirPavlik <damir.pavlik@gmail.com>
2024-05-14 00:15:42 +0200

    feat: added username collection to update profile

--- src/auth/handleUpdateProfile.ts
+++ src/auth/handleUpdateProfile.ts
@@ -1,5 +1,6 @@
-import { auth } from "@/firebase/config";
+import { auth, db } from "@/firebase/config";
 import { updateEmail, updateProfile } from "firebase/auth";
+import { collection, getDocs } from "firebase/firestore";
 
 export async function handleUpdateProfile(formData: FormData) {
   const currentUser = auth.currentUser;
@@ -9,6 +10,15 @@ export async function handleUpdateProfile(formData: FormData) {
   };
   if (currentUser) {
     if (user.username !== "") {
+      const usernameCollection = await getDocs(collection(db, "usernames"));
+      const allUsernames = usernameCollection.docs.map((doc) => ({
+        username: doc.data().username,
+      }));
+      allUsernames.map((item) => {
+        if (item.username === user.username) {
+          throw new Error("Username already exists");
+        }
+      });
       updateProfile(currentUser, {
         displayName: user.username,
       });
